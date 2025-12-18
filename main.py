28f9db787ec4dd4a3ce7bfe60dd872962c07979e
aaronhkim <aaron.hyosik.kim@gmail.com>
2025-12-18 15:34:10 -0800

    finished Ch3L1

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from stats import get_num_words,count_letters
+from stats import get_num_words,count_letters,sorted_count
 def get_book_text(fpath):
     with open(fpath) as f:
         file_contents = f.read()
     return file_contents
 
 def main():
-    words = get_book_text("books/frankenstein.txt")
-    word_count = get_num_words(words)
+    f_path = "books/frankenstein.txt"
+    print("============ BOOKBOT ============")
+    words = get_book_text(f_path)
+    print(f"Analyzing book found at {f_path}...")
     
-    #print(f"Found {word_count} total words")
-    print(count_letters(words))
+    print("----------- Word Count ----------")
+    word_count = get_num_words(words)
+    print(f"Found {word_count} total words")
+    
+    print("--------- Character Count -------")
+    sorted_count(words)
+    #print(sorted_count(words))
 
 main()
--- stats.py
+++ stats.py
@@ -7,17 +7,41 @@ def count_letters(words):
     # ord('z') = 122
     # chr(97) = 'a'
     # chr(122) = 'z'
+    num_ascii_chars = 2**16
     
     # initializing character counter
     letter_count_dict = {}
-    for letter in range(0,2**16):
+    
+    for letter in range(0,num_ascii_chars):
         letter_count_dict[chr(letter)] = 0
     
     # counting letters
     for word in words:
         letter_count_dict[word.lower()] +=1
+        
     # subset containing only required letters
     letters_only_dict = {letter: letter_count_dict[letter] for letter in 
                          [chr(k) for k in range(97,123)]}
+    
     return letters_only_dict
+
+
+def sorted_count(words):
+    def sort_on(items):
+        return items['num']
+    
+    letter_dict= count_letters(words)
+    nums=[]
+    
+    for key in letter_dict:
+        nums.append({"letter":key, "num": letter_dict[key]})
+    
+    nums.sort(reverse=True,key=sort_on)
+    
+    for letter_count in nums:
+        print(f"{letter_count['letter']}: {letter_count['num']}")
+    
+    return nums
+
+
     
