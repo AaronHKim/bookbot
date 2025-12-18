91e6c644babe0f9cddbdfeb112266949d46b9cea
aaronhkim <aaron.hyosik.kim@gmail.com>
2025-12-18 15:06:47 -0800

    finished Ch2L6

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words,count_letters
 def get_book_text(fpath):
     with open(fpath) as f:
         file_contents = f.read()
@@ -8,6 +8,7 @@ def main():
     words = get_book_text("books/frankenstein.txt")
     word_count = get_num_words(words)
     
-    print(f"Found {word_count} total words")
+    #print(f"Found {word_count} total words")
+    print(count_letters(words))
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,22 @@ def get_num_words(words):
     words_array = words.split()
     word_count = len(words_array)
     return word_count
+def count_letters(words):
+    # ord('a') = 97
+    # ord('z') = 122
+    # chr(97) = 'a'
+    # chr(122) = 'z'
+    
+    # initializing character counter
+    letter_count_dict = {}
+    for letter in range(0,2**16):
+        letter_count_dict[chr(letter)] = 0
+    
+    # counting letters
+    for word in words:
+        letter_count_dict[word.lower()] +=1
+    # subset containing only required letters
+    letters_only_dict = {letter: letter_count_dict[letter] for letter in 
+                         [chr(k) for k in range(97,123)]}
+    return letters_only_dict
+    
